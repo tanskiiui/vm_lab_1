b38408dde2bce1c3592eea7708718a1c6860f1d5
tanskiiui <56319101+tanskiiui@users.noreply.github.com>
2020-05-15 21:37:35 +0300

    Add files via upload

---
+++ VM_LAB__num1/VM_LAB__num1/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace VM_LAB__num1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea19 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend19 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea19.AxisX.Title = "X";
+            chartArea19.AxisY.Title = "Y";
+            chartArea19.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea19);
+            legend19.Name = "Legend1";
+            this.chart1.Legends.Add(legend19);
+            this.chart1.Location = new System.Drawing.Point(12, 68);
+            this.chart1.Name = "chart1";
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series19.IsVisibleInLegend = false;
+            series19.Legend = "Legend1";
+            series19.Name = "Series1";
+            this.chart1.Series.Add(series19);
+            this.chart1.Size = new System.Drawing.Size(776, 330);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(37, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(223, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "x(0)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(90, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "x\'(0)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(177, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "delta";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "RUN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(662, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(345, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "h";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(373, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(269, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "b";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(297, 12);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label6.Location = new System.Drawing.Point(294, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "500";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label7.Location = new System.Drawing.Point(220, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "0.1";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label8.Location = new System.Drawing.Point(370, 35);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(22, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "0.1";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label9.Location = new System.Drawing.Point(128, 35);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "0";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label10.Location = new System.Drawing.Point(40, 35);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "10";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(416, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 17);
+            this.checkBox1.TabIndex = 20;
+            this.checkBox1.Text = "Auto select h";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(581, 39);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "SAVE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Метод Рунге-Кутта";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ VM_LAB__num1/VM_LAB__num1/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace VM_LAB__num1
+{
+    public partial class Form1 : Form
+    {
+        double  dx, h, delta, b, a;
+        int n;
+        double[] x;
+        double[] z;
+        public Form1()
+        {
+            InitializeComponent();
+            saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void RunRungeKutt()
+        {
+            x = new double[1000000];
+            z = new double[1000000];
+         
+            x[0] = a;
+            z[0] = 0;
+            if (checkBox1.Checked == true)
+            {
+                h = (b - a) / 5000;
+            }
+            n = Convert.ToInt32((b - a) / h);
+            double T1, T2, T3, T4, U1, U2, U3, U4;
+            for (int i = 0; i < n; i++)
+            {
+                U1 = h * z[i];
+                U2 = h * (z[i] + U1 / 2);
+                U3 = h * (z[i] + U2 / 2);
+                U4 = h * (z[i] + U3);
+
+                T1 = h * f1(x[i], z[i]);
+                T2 = h * f1(x[i] + h / 2, z[i] + T1 / 2);
+                T3 = h * f1(x[i] + h / 2, z[i] + T2 / 2);
+                T4 = h * f1(x[i] + h, z[i] + T3);
+
+            
+                x[i+1] = x[i] + (U1 + 2 * U2 + 2 * U3 + U4) / 6;
+                z[i+1] = z[i] + (T1 + 2 * T2 + 2 * T3 + T4) / 6;
+            }
+            ChartGraph(x, z, n);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            MessageBox.Show("График успешно очищен!", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                textBox3.Enabled = false;
+            }
+            else
+                textBox3.Enabled = true;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = label10.Text;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = label9.Text;
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = label7.Text;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = label6.Text;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = label8.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ReadCheckParams() == 1)
+            {
+                MessageBox.Show("Введены некорректные параметры! Проверьте правильность введенных параметров и попробуйте снова.",
+                    "Предупреждение",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            } 
+            else
+            {
+                RunRungeKutt();
+                RememberParams();
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(n == 0)
+            {
+                MessageBox.Show("Нечего сохранять!", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;       
+            string filename = saveFileDialog1.FileName;
+            string filetext = GetXYvalues(x,z,n);
+            System.IO.File.WriteAllText(filename, filetext);
+            MessageBox.Show("Файл успешно сохранен!","Информация", MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private int ReadCheckParams()
+        {
+            try
+            {
+                a = double.Parse(textBox1.Text, CultureInfo.InvariantCulture.NumberFormat);
+                dx = double.Parse(textBox2.Text, CultureInfo.InvariantCulture.NumberFormat);
+                if(checkBox1.Checked == false)
+                    h = double.Parse(textBox3.Text, CultureInfo.InvariantCulture.NumberFormat);
+                delta = double.Parse(textBox4.Text, CultureInfo.InvariantCulture.NumberFormat);
+                b = double.Parse(textBox5.Text, CultureInfo.InvariantCulture.NumberFormat);
+            }
+            catch
+            {
+                return 1;
+            }
+            return 0;
+        }
+        private double f1(double _x, double _z)
+        {
+            double value;
+            value = -delta * _z - Math.Sin(_x);     
+            return value;
+        }  
+        private void ChartGraph(double[] x, double[] z,int  n)
+        {
+            for(int i = 0; i < n - 1; i++)
+            {  
+                chart1.Series[0].Points.AddXY(Math.Round(x[i],3), z[i]);
+            }
+            
+        }
+        private void RememberParams()
+        {
+            label10.Text = textBox1.Text;
+            label9.Text = textBox2.Text;
+            label7.Text = textBox4.Text;
+            label6.Text = textBox5.Text;
+            label8.Text = textBox3.Text;
+        }
+        private string GetXYvalues(double[] x, double[] y, int n)
+        {
+            string result = "";
+            for(int i = 0; i< n; i++)
+            {
+                result = result.Insert(result.Length, Convert.ToString(Math.Round(x[i],5)));
+                result = result.Insert(result.Length, " ; ");
+                result = result.Insert(result.Length, Convert.ToString(Math.Round(y[i],5)));
+                result = result.Insert(result.Length, System.Environment.NewLine);
+            }
+            return result;
+        }
+    }
+}
